45a8770d08a79c799731570ada19f3dd68b03f85
Sam Waller <samuellwaller@gmail.com>
2015-11-13 04:47:22 -0500

    test

--- spec/features/user_registration_spec.rb
+++ spec/features/user_registration_spec.rb
@@ -7,8 +7,8 @@ describe "user registration", type: :feature, js: true do
     click_link 'Sign up'
     fill_in "Email", with: "hockeywalt@yahoo.com"
     fill_in "Name", with: "Sam"
-    fill_in "Password", with: "Sinch0n12"
-    fill_in "Password confirmation", with: "Sinch0n12"
+    fill_in "Password", with: "password"
+    fill_in "Password confirmation", with: "password"
     click_button "Sign up"
     expect(page).to have_content("Welcome! You have signed up successfully.")
     expect(page).to have_content("Log Out")
